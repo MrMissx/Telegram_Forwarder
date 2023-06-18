7b47dfc3d516323c5ce904ddacc716a62abd7099
Gaung Ramadhan <hi@mrmiss.my.id>
2023-06-18 10:46:19 +0700

    feat: support for topic chat (#82)

--- README.md
+++ README.md
@@ -48,13 +48,15 @@ This file contains the list of chats to forward messages from and to. The bot ex
 [
     {
         "source": -10012345678,
-        "destination": [-10011111111, -10022222222]
+        "destination": [-10011111111, "-10022222222#123456"]
     }
 ]
 ```
 
 -   `source` - The chat ID of the chat to forward messages from. It can be a group or a channel.
+
 -   `destination` - An array of chat IDs to forward messages to. It can be a group or a channel.
+    > Destenation supports Topics chat. You can use `#topicID` string to forward to specific topic. Example: `[-10011111111, "-10022222222#123456"]`. With this config it will forward to chat `-10022222222` with topic `123456` and to chat `-10011111111` .
 
 You may add as many objects as you want. The bot will forward messages from all the chats in the `source` field to all the chats in the `destination` field. Duplicates are allowed as it already handled by the bot.
 
@@ -79,22 +81,22 @@ This will install all necessary python packages.
 #### Requrements
 
 -   Docker
--   docker-compose
+-   docker compose
 
 Before launch make sure all configuration are completed (`.env` and `chat_list.json`)!
 
 Then, simply run the command:
 
 ```shell
-docker-compose up -d
+docker compose up -d
 ```
 
 You can view the logs by the command:
 
 ```shell
-docker-compose logs -f
+docker compose logs -f
 ```
 
 ### Credits
 
--   [saksham2410](https://github.com/saksham2410) - AutoForwarder-TelegramBot
+-   [AutoForwarder-TelegramBot](https://github.com/saksham2410/AutoForwarder-TelegramBot)
--- forwarder/modules/forward.py
+++ forwarder/modules/forward.py
@@ -1,17 +1,19 @@
-from typing import Union
+from typing import Union, Optional
 
 from telegram import Update, Message, MessageId
 from telegram.error import ChatMigrated
 from telegram.ext import MessageHandler, filters, ContextTypes
 
-from forwarder import bot, CONFIG, REMOVE_TAG, LOGGER
-from forwarder.utils import get_source, get_destenation
+from forwarder import bot, REMOVE_TAG, LOGGER
+from forwarder.utils import get_source, get_destenation, parse_topic
 
 
-async def send_message(message: Message, chat_id: int) -> Union[MessageId, Message]:
+async def send_message(
+    message: Message, chat_id: int, thread_id: Optional[int] = None
+) -> Union[MessageId, Message]:
     if REMOVE_TAG:
-        return await message.copy(chat_id)
-    return await message.forward(chat_id)
+        return await message.copy(chat_id, message_thread_id=thread_id)  # type: ignore
+    return await message.forward(chat_id, message_thread_id=thread_id)  # type: ignore
 
 
 async def forwarder(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
@@ -21,20 +23,22 @@ async def forwarder(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
     if not message or not source:
         return
 
-    for chat in get_destenation(source.id, CONFIG):
+    for chat in get_destenation(source.id):
         try:
-            await send_message(message, chat)
+            await send_message(message, chat["chat_id"], thread_id=chat["thread_id"])
         except ChatMigrated as err:
             await send_message(message, err.new_chat_id)
             LOGGER.warning(
                 f"Chat {chat} has been migrated to {err.new_chat_id}!! Edit the config file!!"
             )
-        except Exception:
-            LOGGER.warning(f"Failed to forward message from {source.id} to {chat}")
+        except Exception as err:
+            LOGGER.warning(f"Failed to forward message from {source.id} to {chat} due to {err}")
 
 
 FORWARD_HANDLER = MessageHandler(
-    filters.Chat(get_source(CONFIG)) & ~filters.COMMAND & ~filters.StatusUpdate.ALL,
+    filters.Chat([source["chat_id"] for source in get_source()])
+    & ~filters.COMMAND
+    & ~filters.StatusUpdate.ALL,
     forwarder,
 )
 bot.add_handler(FORWARD_HANDLER)
